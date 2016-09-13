3ad89bb14b527f521b347ffde628d73f30fcf082
djones01 <djones9696@gmail.com>
2016-09-13 01:06:08 -0500

    Working Angular2!!

---
+++ src/GTiHub/Controllers/API/UserController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GTiHub.Models.EntityModel;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GTiHub.Controllers.API
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private readonly GTiHubContext dbContext;
+        public UserController(GTiHubContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        // GET: api/User
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return dbContext.Users.ToList();
+        }
+
+        // GET api/User/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+ 
+        // POST api/User
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/User/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/User/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/GTiHub/gulpfile.js
@@ -0,0 +1,47 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+
+var paths = {};
+paths.npmSrc = "./node_modules/";
+paths.npmLibs = "./wwwroot/lib/npmlibs/";
+
+gulp.task("copy-deps:systemjs", function () {
+    return gulp.src(paths.npmSrc + '/systemjs/dist/**/*.*', { base: paths.npmSrc + '/systemjs/dist/' })
+         .pipe(gulp.dest(paths.npmLibs + '/systemjs/'));
+});
+
+gulp.task("copy-deps:angular2", function () {
+    return gulp.src(paths.npmSrc + '/angular2/bundles/**/*.js', { base: paths.npmSrc + '/angular2/bundles/' })
+         .pipe(gulp.dest(paths.npmLibs + '/angular2/'));
+});
+
+gulp.task("copy-deps:es6-shim", function () {
+    return gulp.src(paths.npmSrc + '/es6-shim/es6-sh*', { base: paths.npmSrc + '/es6-shim/' })
+         .pipe(gulp.dest(paths.npmLibs + '/es6-shim/'));
+});
+
+gulp.task("copy-deps:es6-promise", function () {
+    return gulp.src(paths.npmSrc + '/es6-promise/es6-pr*', { base: paths.npmSrc + '/es6-promise/' })
+         .pipe(gulp.dest(paths.npmLibs + '/es6-promise/'));
+});
+
+gulp.task("copy-deps:rxjs", function () {
+    return gulp.src(paths.npmSrc + '/rxjs/bundles/*.*', { base: paths.npmSrc + '/rxjs/bundles/' })
+         .pipe(gulp.dest(paths.npmLibs + '/rxjs/'));
+});
+
+gulp.task("copy-deps:reflect-metadata", function () {
+    return gulp.src(paths.npmSrc + '/reflect-metadata/reflect.js', { base: paths.npmSrc + '/reflect-metadata/' })
+         .pipe(gulp.dest(paths.npmLibs + '/reflect-metadata/'));
+});
+
+gulp.task("copy-deps:corejs", function () {
+    return gulp.src(paths.npmSrc + '/corejs/**/*.js', { base: paths.npmSrc + '/corejs/**/*.js' })
+         .pipe(gulp.dest(paths.npmLibs + '/corejs/'));
+});
+
+gulp.task("copy-deps", ["copy-deps:rxjs", 'copy-deps:angular2', 'copy-deps:systemjs', 'copy-deps:es6-shim','copy-deps:es6-promise','copy-deps:reflect-metadata','copy-deps:corejs']);
