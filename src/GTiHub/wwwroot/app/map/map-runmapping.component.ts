d2410be26d82603aa07fd6b13739eec53e230879
djones01 <djones9696@gmail.com>
2016-10-18 00:21:25 -0500

    Minor changes Committing before attempting to work on the file upload plugin.

---
+++ src/GTiHub/wwwroot/app/map/map-runmapping.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'map-runmapping',
+    templateUrl: 'map-runmapping.component.html'
+})
+export class RunMappingComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
